1a84eab3ff254f332d688f4d3a518b5e54421108
Ankur Singh <68726977+ankur2802@users.noreply.github.com>
2025-06-26 01:06:38 +0530

    Delete SpringJDBC directory

--- SpringJDBC/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into student (rollNo, name, marks) values (101, 'kiran', 78);
-insert into student (rollNo, name, marks) values (102, 'suraj', 79);
-insert into student (rollNo, name, marks) values (103, 'sanjay', 80);
--- SpringJDBC/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-create table student( rollNo int primary key,
-name varchar(50),
-marks int
-);
-
